364bdc6d7aac72a73a83de4d9c512a7dc4224311
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-04-25 19:33:05 -0300

    feat: create restaurants view & controller

---
+++ lib/inmana_web/controllers/restaurants_controller.ex
@@ -0,0 +1,18 @@
+defmodule InmanaWeb.RestaurantsController do
+  use InmanaWeb, :controller
+
+  alias Inmana.Restaurant
+  alias Inmana.Restaurants.Create
+
+  alias InmanaWeb.FallbackController
+
+  action_fallback FallbackController
+
+  def create(connection, params) do
+    with {:ok, %Restaurant{} = restaurant} <- Create.call(params) do
+      connection
+      |> put_status(:created)
+      |> render("create.json", restaurant: restaurant)
+    end
+  end
+end
