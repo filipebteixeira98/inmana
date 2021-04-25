305e532779c6f604f1798301f68a12b26e1a9bc8
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-04-25 16:30:44 -0300

    feat: Add welcome router & controller

--- lib/inmana/welcomer.ex
+++ lib/inmana/welcomer.ex
@@ -1,18 +1,30 @@
 defmodule Inmana.Welcomer do
-  def welcome(params) do
-    age = params["age"]
+  def welcome(%{"name" => name, "age" => age}) do
+    age = String.to_integer(age)
 
-    name =
-      params["name"]
-      |> String.trim()
-      |> String.downcase()
+    name
+    |> String.trim()
+    |> String.downcase()
+    |> evaluate(age)
 
     # |> IO.inspect()
 
-    if name == "Filipe Teixeira" and age == "23" do
-      IO.puts("Hello, ", name)
-    else
-      IO.puts("You are not allowed to be here!")
-    end
+    # if name == "Filipe Teixeira" and age == "23" do
+    #   IO.puts("Hello, ", name)
+    # else
+    #   IO.puts("You are not allowed to be here!")
+    # end
+  end
+
+  defp evaluate("ethan", 23) do
+    {:ok, "Hello, Ethan"}
+  end
+
+  defp evaluate(name, age) when age >= 18 do
+    {:ok, "Welcome, #{name}!"}
+  end
+
+  defp evaluate(name, _age) do
+    {:error, "You shall not pass, #{name}"}
   end
 end
